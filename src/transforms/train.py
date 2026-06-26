e2fc91cd67136bce57a21d18d547a1356a611edf
Sergey Pavlov <saleksepavlov@sberbank.ru>
2026-06-26 16:18:50 +0300

    RandomAffine + ElasticTransform

--- src/transforms/train.py
+++ src/transforms/train.py
@@ -5,7 +5,7 @@ from .common import DEFAULT_TRANSFORMS
 
 def get_train_transform() -> transforms.Compose:
     transforms_ = [
-        # transforms.RandomAffine(degrees=10, translate=(0.08, 0.08), scale=(0.9, 1.1), shear=5,),
+        transforms.RandomAffine(degrees=10, translate=(0.08, 0.08), scale=(0.9, 1.1), shear=5,),
         transforms.ElasticTransform(alpha=20.0, sigma=4.0,),
     ]
     transforms_.extend(DEFAULT_TRANSFORMS)
